60d3313ebafefc9109c7a2f30e92415b32a491ea
MyBrokeAss <42857913+MyBrokeAss@users.noreply.github.com>
2018-12-30 17:40:49 -0700

    Update and rename Velocity.py to Mashup.py

--- Mashup.py
+++ Mashup.py
@@ -1,19 +1,19 @@
-import time
+from time import sleep
 
 print("kph", "knots", "mph", "feet/sec")
-time.sleep(2)
+sleep(2)
 for kph in range(0, 1000, 10):
-    mph = int(kph*0.62)
-    kts = int(kph*0.62*1/1.15)
-    fps = int(kph*0.62*88/60)
+    mph = float(kph*0.62)
+    kts = float(kph*0.62*1/1.15)
+    fps = float(kph*0.62*88/60)
     print(kph, "kph", kts, "knots", mph, "mph", fps, "feet/sec")
-    time.sleep(0.15)
+    sleep(1)
     
 
 print("\n...................................................")
 
 
-time.sleep(4)
+sleep(4)
 
 cel = int(-273)
 
@@ -21,10 +21,10 @@ for cel in range (-273,501,25):
     fah = int((cel * 1.8) + 32)
     kel = int(cel + 273)
     print(cel, "Celcius", fah, "Fahrenheit", kel, "Kelvin")
-    time.sleep(0.25)
+    sleep(0.25)
 
 print("\n...................................................")
-time.sleep(4)
+sleep(4)
 
 mortgage = int(122500)
 sale_price = int(input("Sale price? "))
@@ -33,16 +33,16 @@ commission = int(0.055 * sale_price)
 sale_costs = int(0.01*sale_price)
 print("\n...................................................")
 
-time.sleep(3)
+sleep(3)
 print("Sale price of $", sale_price)
-time.sleep(1)
+sleep(1)
 print("\nwith a total cost of sale of $",  commission + sale_costs)
-time.sleep(1)
+sleep(1)
 print("\nand a mortgage payoff of $", mortgage)
-time.sleep(2)
+sleep(2)
 print("\nresults in a net payout of: $", sale_price - commission - sale_costs - mortgage)
 
 print("\n...................................................")
-time.sleep(3)
+sleep(3)
 
 input("\n\nPress any key to exit.")
