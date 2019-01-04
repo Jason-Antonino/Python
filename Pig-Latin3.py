80bd9165d353a52e0843bdf8301b85c460344f57
MyBrokeAss <42857913+MyBrokeAss@users.noreply.github.com>
2019-01-03 21:36:54 -0700

    Update Pig-Latin3.py

--- Pig-Latin3.py
+++ Pig-Latin3.py
@@ -1,4 +1,4 @@
-import time #this imports the time module
+from time import sleep #this imports the sleep function from the time module
 import datetime #this imports the datetime module
 
 curr = datetime.datetime.now()
@@ -6,7 +6,7 @@ print("Today is ", curr.strftime("%B"), curr.strftime("%d"), ',', curr.strftime(
 
 #Data Entry---------------------------------------------
 sentence = input("Enter a word or sentence: ")
-time.sleep(1) #wait one second
+sleep(1) #wait one second
 print("You entered: " + sentence + ".")
 
 #Analysis------------------------------------------------
@@ -16,17 +16,17 @@ numwords = sentence.count(' ', 0, span)+1
 this counts the number of spaces in the sentence starting from the first character of the first word and stopping at the last character of the last word, plus one, because there is no space after the end of the sentence
 """
 
-time.sleep(2)
+sleep(2)
 
 print("Your entry has", numwords, "words and is", span,"characters in length.")
 
-time.sleep(2)
+sleep(2)
 
 print("\nNow we're going to translate your verbiage into Pig Latin.")
-time.sleep(2)
+sleep(2)
 
-for x in range(0, 2):
-    print(" ")  #prints blank space two times
+for x in range(0, 3):
+    print(" ")  #prints blank space 3 times
 
 #DataCleaning------------------------------------
 sentence = sentence.replace(',', '') #removes commas
@@ -39,7 +39,7 @@ for word in sentence: # this loop prints each word starting with the second char
         word = word + '%say' % (word[0]) #modify the word to append the first character (word[0]) and "ay" to the end
         word = word[1: ] #then modifying the modified word to start with the second character (word[1]) and continue to the last character (word[])
         print(word.lower(), end = ' ') #print the new Pig Latin word in lowercase plus a space at the end, and append the next word to this word until the loop is finished
-        time.sleep(0.5)
+        sleep(0.5)
         
     elif len(word) == 1:
         print(word, end = ' ') #if a word is a single character then just print it out with a space at the end and keep going until the loop is finished
