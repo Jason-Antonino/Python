7dda396b7b25159db0cf098d61d983d382749012
MyBrokeAss <42857913+MyBrokeAss@users.noreply.github.com>
2019-01-03 21:15:43 -0700

    Update NHL_Playoffs.py

--- NHL_Playoffs.py
+++ NHL_Playoffs.py
@@ -1,5 +1,6 @@
 # Best of Seven NHL Playoff Series
 
+from time import sleep
 from random import randrange
 
 team1 = input("Enter first team name: ")
@@ -8,36 +9,40 @@ team2 = input("Enter second team name: ")
 counta = 0
 countb = 0
 
+print("")
 for n in range(0, 7):
     x = randrange(0, 11)
     y = randrange(0, 11)
     
     if x == y:
-        r = randrange(1, 2)
+        r = randrange(0, 2)
         if r == 1:
-            print(team1, x+1, team2, y, "(OT)")
-            print("Team 1 wins in overtime!")
+            print(team1, x+1, ",", team2, y, "(OT)")
+            print(team1, "win in overtime!")
             counta += 1
         elif r == 2:
-            print(team2, y+1, team1, x, "(OT)")
-            print("Team 2 wins in overtime!")
+            print(team2, y+1, ",", team1, x, "(OT)")
+            print(team2, "win in overtime!")
             countb += 1
         
     if x > y:
-        print(team1, x, team2, y)
-        print("Team 1 wins!")
+        print(team1, x, ",", team2, y)
+        print(team1 + " win!")
         counta += 1
         
     elif y > x:
-        print(team2, y, team1, x)
-        print("Team 2 wins!")
+        print(team2, y, ",", team1, x)
+        print(team2 + " win!")
         countb += 1
 
     if counta == 4:
-        print("\n", team1, "wins the series", counta, "games to", countb)
+        print("")
+        print(team1, "win the series", counta, "games to", countb)
         break
     elif countb == 4:
-        print("\n", team2, "wins the series", countb, "games to", counta)
+        print("")
+        print(team2, "win the series", countb, "games to", counta)
         break
     
     print("")
+    sleep(1)
