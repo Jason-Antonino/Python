e661725de3b0219198b229df4840b2e20515667c
MyBrokeAss <42857913+MyBrokeAss@users.noreply.github.com>
2019-02-18 22:55:39 -0700

    Add files via upload

--- Pig-Latin3.py
+++ Pig-Latin3.py
@@ -1,32 +1,35 @@
-from time import sleep #this imports the sleep function from the time module
+import time #this imports the time module
 import datetime #this imports the datetime module
 
 curr = datetime.datetime.now()
-print("Today is ", curr.strftime("%B"), curr.strftime("%d"), ',', curr.strftime("%Y"))
+print("Today is " + curr.strftime("%B") + " " + curr.strftime("%d") + ',' + " " + curr.strftime("%Y"))
 
 #Data Entry---------------------------------------------
 sentence = input("Enter a word or sentence: ")
-sleep(1) #wait one second
+time.sleep(1) #wait one second
 print("You entered: " + sentence + ".")
 
 #Analysis------------------------------------------------
 span = len(sentence)
 numwords = sentence.count(' ', 0, span)+1
-"""
-this counts the number of spaces in the sentence starting from the first character of the first word and stopping at the last character of the last word, plus one, because there is no space after the end of the sentence
-"""
 
-sleep(2)
+'''
+#this counts the number of spaces in the sentence 
+#starting from the first character of the first word and stopping at the last character of the last word, plus one, 
+#because there is no space after the end of the sentence
+'''
+
+time.sleep(2)
 
 print("Your entry has", numwords, "words and is", span,"characters in length.")
 
-sleep(2)
+time.sleep(2)
 
 print("\nNow we're going to translate your verbiage into Pig Latin.")
-sleep(2)
+time.sleep(2)
 
-for x in range(0, 3):
-    print(" ")  #prints blank space 3 times
+for x in range(2):
+    print(" ")  #prints blank space two times
 
 #DataCleaning------------------------------------
 sentence = sentence.replace(',', '') #removes commas
@@ -36,10 +39,10 @@ sentence = sentence.split(' ') # break down sentence into individual words using
 #Reporting------------------------------------------------------------------------------------------------------------------------
 for word in sentence: # this loop prints each word starting with the second character and appends the first character and "ay" to the end of the word until the last word has been changed
     if len(word) >=2:
-        word = word + '%say' % (word[0]) #modify the word to append the first character (word[0]) and "ay" to the end
+        word = word + word[0] + "ay" #modify the word to append the first character (word[0]) and "ay" to the end
         word = word[1: ] #then modifying the modified word to start with the second character (word[1]) and continue to the last character (word[])
         print(word.lower(), end = ' ') #print the new Pig Latin word in lowercase plus a space at the end, and append the next word to this word until the loop is finished
-        sleep(0.5)
+        time.sleep(0.5)
         
     elif len(word) == 1:
         print(word, end = ' ') #if a word is a single character then just print it out with a space at the end and keep going until the loop is finished
