065be5d4567d1c1b6f1809d6b3781e4854b5c4c0
Jason Antonino <42857913+Jason-Antonino@users.noreply.github.com>
2019-03-03 12:15:26 -0700

    Add files via upload

--- Fuel-cost-converter3.py
+++ Fuel-cost-converter3.py
@@ -8,21 +8,26 @@ sleep(1)
 c = CurrencyRates()
 dict = c.get_rates('USD') #dictionary of countries and conversion factors
 key = input("Enter 3-character code for the country you want (e.g. MXN): ")
+key = key.upper() #forces uppercase lettering
 
 exchrate = dict[key] #cross-reference country code with conversion factor in dictionary
 exchrate = float(exchrate)
+
 gas_price = input("Enter the price per liter of gas in foreign currency: ")
 gas_price = float(gas_price)
+
 liters = 1.00
 gallons = 3.785 * liters
 
 usdollars = gas_price / exchrate
-convprice = usdollars * gallons    
+
+convprice = usdollars * gallons
+convprice = round(convprice, 3)  
 
 sleep(2)
 
 #Output ---------------------------------------->
-print("\nYour price is $", round(convprice, 2), "per US gallon.")
+print("\nYour price is", '${:,.3f}'.format(convprice), "per US gallon.")
 sleep(1)
-print("There are", exchrate, "foreign currency units per US dollar.")
+print("There are", exchrate, key, "per US dollar.")
 input("\n\nPress any key to exit.")
