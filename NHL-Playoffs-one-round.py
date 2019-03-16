a997101687ede73df00ad2736cbb15c8a19dafc8
Jason Antonino <42857913+Jason-Antonino@users.noreply.github.com>
2019-03-15 23:07:59 -0700

    Update NHL-Playoffs-one-round.py

--- NHL-Playoffs-one-round.py
+++ NHL-Playoffs-one-round.py
@@ -21,6 +21,7 @@ for n in range(7): #defines a best-of-seven series starting from game zero and f
         counta += 1 #Team1 gains one win in the series
         sleep(2)
         print("")
+   
     elif y > x: #Team 2 wins the game
         print(team2, y, ",", team1, x)
         print(team2 + " win!")
@@ -32,14 +33,25 @@ for n in range(7): #defines a best-of-seven series starting from game zero and f
         otwinnr = randrange(2) #picks a zero or one to decide overtime winner
         otnum = randrange(1,5) #number of overtime periods ranging from 1 to 4
         if otwinnr == 0:
-            print(team1, x+1, ",", team2, y, " ", "(", otnum,"OT )") #Team1 wins in overtime 
-            print(team1, "win in overtime number", otnum)
+            if otnum == 1:
+                print(team1, x+1, ",", team2, y, "(OT)") #Team1 wins in OT
+                print(team1, "win in overtime")
+            
+            elif otnum != 1:
+                print(team1, x+1, ",", team2, y, " ", "(", otnum,"OT )") #Team1 wins in overtime 
+                print(team1, "win in overtime number", otnum)
             counta += 1
             sleep(2)
             print("")
+        
         elif otwinnr == 1:
-            print(team2, y+1, ",", team1, x, " ", "(", otnum,"OT )") #Team2 wins in overtime
-            print(team2, "win in overtime number", otnum)
+            if otnum == 1:
+                print(team2, y+1, ",", team1, x, "(OT)")
+                print(team2, "win in overtime")
+            
+            elif otnum != 1:
+                print(team2, y+1, ",", team1, x, " ", "(", otnum,"OT )") #Team2 wins in overtime 
+                print(team2, "win in overtime number", otnum)
             countb += 1
             sleep(2)
             print("")
