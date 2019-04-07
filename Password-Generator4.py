563bd27ef83bddba60e7ec79f344a89bdb491283
Jason Antonino <42857913+Jason-Antonino@users.noreply.github.com>
2019-04-07 14:29:14 -0700

    Add files via upload

--- Password-Generator4.py
+++ Password-Generator4.py
@@ -1,7 +1,7 @@
 from random import randrange
 from time import sleep
 password = [] #empty list to store created password
-random_character = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", "/", ",", ".", "<", ">", "?", ";", ":", "\\") #tuple of password character choices
+random_character = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", "/", ",", ".", "<", ">", "?", ";", ":", "\\") #tuple of password character choices
 
 def get_info(password):
     length = input("How many characters for your password?: ")
@@ -9,15 +9,18 @@ def get_info(password):
     run_loop(length, password) #calls the run_loop function and passes the length and empty password to it
 
 def run_loop(length, password):
-    for num in range(length): #starts a loop that will repeat for each character, the number of loops equal to the length of the password
-        x = randrange(66) #picks a random number between zero and 65, inclusive
-        password.append(random_character[x]) #locates the character in the dictionary at the corresponding key number and appends the character to the list
+    num = 1
+    while num <= length: #a loop that executes while num is less than or equal to the length of the password
+        x = randrange(65) #picks a random number between zero and 64, inclusive
+        password.append(random_character[x]) #locates the character in the tuple at the corresponding key number and appends the character to the list
+        num += 1 #increments counter by 1
+    
     output(length, password) #calls the output function and passes the length and new password to it
 
 def output(length, password):
     password = ''.join(map(str, password)) #removes list formatting by converting the list to a string and then replacing the brackets and commas with null values
     sleep(1)
-    print("Your new password is: ", password)
+    print("\nYour new password is: ", password)
     sleep(0.5)
     sat = input("Are you satisfied (Y/N)?: ")
 
